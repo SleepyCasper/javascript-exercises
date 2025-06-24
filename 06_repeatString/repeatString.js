9667ab496ad1178e11038d00954c1bf5302d4c97
Sofia <117224812+SleepyCasper@users.noreply.github.com>
2025-06-24 15:08:16 +0300

    Do 6-11 assignments

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, count) {
+    let output = "";
+    
+    for (let i = 0; i < count; i++) {
+        output += string;
+    }
 
+    return output;
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...itemsToRemove) {
+    return array.filter(item => !itemsToRemove.includes(item));
 };
 
+removeFromArray([1, 2, 3, 4], 3);
 // Do not edit below this line
 module.exports = removeFromArray;
