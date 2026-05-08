f611771d04e899d8b08958e8f5313edc70adbe37
Sofia <117224812+SleepyCasper@users.noreply.github.com>
2026-05-08 13:36:25 +0300

    do 12-16 assignments

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.replace(/[^a-z0-9]/gi, "").toLowerCase();
+    let reversedString = newString.split('').reverse().join('');
+    return reversedString === newString;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    let prev = 1;
+    let current = 1;
+    let result = 0;
+    n = +n;
 
+    if (n > 2) {
+        for (let i = 3; i <= n; i++) {
+            result = prev + current;
+            prev = current;
+            current = result;
+        }
+        return current;
+    } else if (n > 0 && n <= 2) {
+        return 1;
+    } else if (n === 0) {
+        return n;
+    } else return "OOPS";
 };
 
 // Do not edit below this line
